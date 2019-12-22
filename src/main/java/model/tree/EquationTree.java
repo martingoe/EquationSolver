5c4f56590383ff7885a03b9e33b9cd51121332b0
xCubeArrow <1984flaker@gmail.com>
2019-12-22 20:19:39 +0100

    Created a function that applies a operation to the nodes of an EquationTree

--- src/main/java/model/tree/EquationTree.java
+++ src/main/java/model/tree/EquationTree.java
@@ -2,21 +2,58 @@ package model.tree;
 
 import model.operations.Operation;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
 
+@SuppressWarnings("rawtypes")
 public class EquationTree extends Node<Operation> {
-
-    Node<Variable>[] getVariables(Node[] childrenNodes){
-        ArrayList<Node<Variable>> vars = new ArrayList<>();
-        for (int i = 0; i < childrenNodes.length; i++) {
-
-        }
-        return null;
-    }
-
 
     public EquationTree(List<Node<Operation>> childrenNodes) {
         super(childrenNodes);
+    }
+
+    public void applyOperationToEveryNumberOnAllBaseNodes(Class operationClass, float number, List<Node> nodesToExclude) {
+        childrenNodes.forEach(childrenNode -> applyOperationOnNode(childrenNode, operationClass, number, nodesToExclude));
+    }
+
+    @SuppressWarnings("unchecked")
+    private void applyOperationOnNode(Node node, Class operationClass, float number, List<Node> nodesToExclude) {
+        // Initializes a List of new the new Nodes that are included in the parent Node
+        List<Node> newChildrenNodes = new ArrayList();
+
+        for (int i = 0; i < node.childrenNodes.size(); i++) {
+            Object childrenNode = node.childrenNodes.get(i);
+            // Continues with the next iteration if the children node should be excluded
+            if (nodesToExclude.contains(childrenNode)) continue;
+            // Insert a new operation with the new node as the first parameter
+            try {
+                List elements = new ArrayList<>() {{
+                    add(childrenNode);
+                    add(number);
+                }};
+                // Add the new element to the List of new Nodes
+                newChildrenNodes.add((Node) operationClass.getDeclaredConstructor(List.class).newInstance(elements));
+            } catch (InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+                e.printStackTrace();
+            }
+
+        }
+        // Set the new children nodes to the parent node
+    node.setChildrenNodes(newChildrenNodes);
+    }
+
+    Variable[] getVariables(List<Node> childrenNodes) {
+        ArrayList<Variable> vars = new ArrayList<>();
+        for (Node childrenNode : childrenNodes) {
+            if (childrenNode instanceof Variable) {
+                vars.add((Variable) childrenNode);
+            } else if (childrenNode instanceof Operation) {
+                for (Node variable : getVariables(childrenNode.getChildrenNodes())) {
+                    vars.add((Variable) variable);
+                }
+            }
+        }
+        return vars.toArray(new Variable[0]);
     }
 }
