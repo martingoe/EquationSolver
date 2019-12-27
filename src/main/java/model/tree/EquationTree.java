9fb815492693f9519d1a00a547c5301d58804d9a
xCubeArrow <1984flaker@gmail.com>
2019-12-27 14:57:22 +0100

    Added documentation for the EquationTree class. Also let the getVariables function return a HashMap with the parentNode

--- src/main/java/model/tree/EquationTree.java
+++ src/main/java/model/tree/EquationTree.java
@@ -4,33 +4,42 @@ import model.operations.Operation;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @SuppressWarnings("rawtypes")
-public class EquationTree extends Node<Operation> {
+public class EquationTree extends Node {
 
-    public EquationTree(List<Node<Operation>> childrenNodes) {
+    /**
+     * Initializes an {@link EquationTree}. This is what saves an equation
+     * @param childrenNodes The nodes that are contained in the equation. Usually 2 elements long
+     */
+    public EquationTree(List<Node> childrenNodes) {
         super(childrenNodes);
     }
 
-    public void applyOperationToEveryNumberOnAllBaseNodes(Class operationClass, float number, List<Node> nodesToExclude) {
-        childrenNodes.forEach(childrenNode -> applyOperationOnNode(childrenNode, operationClass, number, nodesToExclude));
-    }
 
+    /**
+     * Applies an operation on every children node of the equation
+     *
+     * @param operationClass          the {@link java.lang.Class} of the {@link Operation} that is supposed to be applied
+     * @param secondObjectOfOperation The secondObjectOfOperation that is applied to the {@link Operation}
+     * @param nodesToExclude          A {@link java.util.List} of {@link Node} that should be excluded from the change
+     */
     @SuppressWarnings("unchecked")
-    private void applyOperationOnNode(Node node, Class operationClass, float number, List<Node> nodesToExclude) {
+    public void applyOperationToNodes(Class operationClass, Object secondObjectOfOperation, List<Node> nodesToExclude) {
         // Initializes a List of new the new Nodes that are included in the parent Node
         List<Node> newChildrenNodes = new ArrayList();
 
-        for (int i = 0; i < node.childrenNodes.size(); i++) {
-            Object childrenNode = node.childrenNodes.get(i);
+        for (int i = 0; i < childrenNodes.size(); i++) {
+            Object childrenNode = childrenNodes.get(i);
             // Continues with the next iteration if the children node should be excluded
             if (nodesToExclude.contains(childrenNode)) continue;
             // Insert a new operation with the new node as the first parameter
             try {
                 List elements = new ArrayList<>() {{
                     add(childrenNode);
-                    add(number);
+                    add(secondObjectOfOperation);
                 }};
                 // Add the new element to the List of new Nodes
                 newChildrenNodes.add((Node) operationClass.getDeclaredConstructor(List.class).newInstance(elements));
@@ -40,20 +49,31 @@ public class EquationTree extends Node<Operation> {
 
         }
         // Set the new children nodes to the parent node
-    node.setChildrenNodes(newChildrenNodes);
+        setChildrenNodes(newChildrenNodes);
     }
 
-    Variable[] getVariables(List<Node> childrenNodes) {
-        ArrayList<Variable> vars = new ArrayList<>();
-        for (Node childrenNode : childrenNodes) {
+    /**
+     * Gets the {@link Variable}s with the {@link Node}s that are in the equation
+     * <p>
+     * This function is recursive and hence needs a {@link List} of {@link Variable}s as a Argument
+     *
+     * @param parentNode The current {@link Node} that of the recursion
+     * @return Returns the {@link Variable}s as a {@link HashMap}
+     */
+    public HashMap<Variable, Node> getVariables(Node parentNode) {
+        HashMap<Variable, Node> vars = new HashMap<>();
+        // Iterate over the children nodes in order to check them all
+        for (Node childrenNode : parentNode.getChildrenNodes()) {
+            // Add the node to the List of Variables
             if (childrenNode instanceof Variable) {
-                vars.add((Variable) childrenNode);
-            } else if (childrenNode instanceof Operation) {
-                for (Node variable : getVariables(childrenNode.getChildrenNodes())) {
-                    vars.add((Variable) variable);
-                }
+                vars.put((Variable) childrenNode, parentNode);
+            }
+            // If the children node is a operation, add the variables in that operation
+            else if (childrenNode instanceof Operation) {
+                vars.putAll(getVariables(childrenNode));
             }
         }
-        return vars.toArray(new Variable[0]);
+        // Return the list of Variables
+        return vars;
     }
 }
