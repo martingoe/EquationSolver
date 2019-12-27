0e6c31c221e3a38c8affa99a4b68539655a92db8
xCubeArrow <1984flaker@gmail.com>
2019-12-27 14:54:29 +0100

    Removed the Type from the Node as it was not necessary.

--- src/main/java/model/tree/Node.java
+++ src/main/java/model/tree/Node.java
@@ -2,34 +2,34 @@ package model.tree;
 
 import java.util.List;
 
-public class Node<T> {
-    List<Node<T>> childrenNodes;
+public class Node {
+    List<Node> childrenNodes;
 
-    public Node(List<Node<T>> childrenNodes) {
+    public Node(List<Node> childrenNodes) {
         this.childrenNodes = childrenNodes;
     }
 
-    public List<Node<T>> removeChildrenNode(Node<T> nodeToBeRemoved) {
+    public List<Node> removeChildrenNode(Node nodeToBeRemoved) {
         this.childrenNodes.remove(nodeToBeRemoved);
         return childrenNodes;
     }
 
-    public List<Node<T>> removeChildrenNode(int index) {
+    public List<Node> removeChildrenNode(int index) {
         this.childrenNodes.remove(index);
         return childrenNodes;
     }
 
-    public List<Node<T>> insertChildrenNode(Node<T> nodeToBeInserted) {
+    public List<Node> insertChildrenNode(Node nodeToBeInserted) {
         this.childrenNodes.add(nodeToBeInserted);
 
         return this.childrenNodes;
     }
 
-    public List<Node<T>> getChildrenNodes() {
+    public List<Node> getChildrenNodes() {
         return childrenNodes;
     }
 
-    public void setChildrenNodes(List<Node<T>> childrenNodes) {
+    public void setChildrenNodes(List<Node> childrenNodes) {
         this.childrenNodes = childrenNodes;
     }
 }
