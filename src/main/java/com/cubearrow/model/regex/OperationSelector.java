7004ec312650b20869b78a8d0b9ccbcb9d99343d
xCubeArrow <1984flaker@gmail.com>
2020-02-14 07:10:48 +0100

    Make the OperationSelector work with the classes in the package and not with a pre specified amount.

--- src/main/java/com/cubearrow/model/regex/OperationSelector.java
+++ src/main/java/com/cubearrow/model/regex/OperationSelector.java
@@ -1,18 +1,27 @@
 package com.cubearrow.model.regex;
 
-import com.cubearrow.model.operations.*;
+import com.cubearrow.model.operations.Operation;
+import org.reflections.Reflections;
 
 import java.util.HashMap;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class OperationSelector {
 
 
-    public static Class getOperationFromOperationString(char operation) {
-        HashMap<Character, Class> operationHashMap = new HashMap<>();
-        operationHashMap.put('+', Addition.class);
-        operationHashMap.put('-', Subtraction.class);
-        operationHashMap.put('*', Multiplication.class);
-        operationHashMap.put('/', Division.class);
+    public static Class getOperationFromOperationString(String operation) {
+        HashMap<String, Class> operationHashMap = new HashMap<>();
+        Reflections reflection = new Reflections("com.cubearrow.model.operations");
+        Set<Class<? extends Operation>> operationClasses = reflection.getSubTypesOf(Operation.class);
+        operationClasses.forEach(operationClass -> {
+            try {
+                operationHashMap.put((String) operationClass.getDeclaredField("OPERATION_STRING").get(null), operationClass);
+            } catch (NoSuchFieldException | IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        });
+
 
         return operationHashMap.get(operation);
     }
