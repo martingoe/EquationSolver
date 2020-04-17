ea0a5b004571d826ded2b022e4edd429d5e92ac1
xCubeArrow <1984flaker@gmail.com>
2020-04-17 23:37:51 +0200

    Clean up RegExUtilities.java

--- model/src/main/java/com/cubearrow/model/regex/RegExUtilities.java
+++ model/src/main/java/com/cubearrow/model/regex/RegExUtilities.java
@@ -5,13 +5,21 @@ import java.util.regex.Pattern;
 
 public class RegExUtilities {
 
-    public static Integer getFirstSubstring(String baseString, String regExString, int startIndex) {
-        Pattern pattern = Pattern.compile(regExString);
+    /**
+     * Returns the starting index of the first substring found in a string with a RegEx expression
+     * @param baseString The string that is matched and whose index of the substring is returned
+     * @param regexExpression The RegEx expression that is used to match the baseString
+     * @param startIndex The index at which the search starts.
+     *                   This enables the user to leave out parts before the part to be searched
+     * @return Returns the index of the first substring, if there is no substring, returns -1
+     */
+    public static int getStartingIndexOfFirstSubstring(String baseString, String regexExpression, int startIndex) {
+        Pattern pattern = Pattern.compile(regexExpression);
         Matcher matcher = pattern.matcher(baseString);
 
         if (matcher.find(startIndex)) {
             return matcher.start();
         }
-        return null;
+        return -1;
     }
 }
