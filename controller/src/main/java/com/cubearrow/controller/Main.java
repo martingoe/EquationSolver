4647c657f15dd1bb217f808e29ee72424cf3db2c
xCubeArrow <1984flaker@gmail.com>
2020-04-25 15:56:26 +0200

    Update Main.java to apply the new changes

--- controller/src/main/java/com/cubearrow/controller/Main.java
+++ controller/src/main/java/com/cubearrow/controller/Main.java
@@ -2,21 +2,23 @@ package com.cubearrow.controller;
 
 import com.cubearrow.model.equation.Equation;
 import com.cubearrow.model.equation.EquationInitializer;
-import com.cubearrow.model.regex.OperationSelector;
+import com.cubearrow.model.operations.Operation;
+import com.cubearrow.model.rewriting.EquationRewriter;
+import com.cubearrow.model.tree.Variable;
 import com.cubearrow.view.Interaction;
 
 public class Main {
     public static void main(String[] args) {
-        Equation test = new EquationInitializer("((3/4)*x)+x=y").parseEquation();
+        Equation test = new EquationInitializer("((3/4)*x)+x=2+x").parseEquation();
+
         Interaction interaction = new Interaction(test);
-        interaction.askUserForVariableToIsolate();
-        Equation previous = null;
-        while (!test.equals(previous)) {
-            previous = (Equation) test.clone();
-            test.simplify();
+        test.setVariableToIsolate(interaction.askUserForVariableToIsolate());
+        EquationRewriter equationRewriter = new EquationRewriter();
+        for (int i = 0; i < 20; i++) {
+            test = test.simplify(equationRewriter);
         }
         interaction.displayResult(test);
-        OperationSelector.sortClassesByPriority(new OperationSelector().getOperationHashMap().values().toArray(new Class[0]));
+        //OperationSelector.sortClassesByPriority(operationSelector.getOperationHashMap().values().toArray(new Class[0]));
 
         System.out.println("DEBUG");
     }
