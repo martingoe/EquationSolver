528ba57cf9d4ee24322aaafd6dc6f67f4399c360
xCubeArrow <1984flaker@gmail.com>
2020-05-01 15:55:53 +0200

    Clean up OperationSelector

--- model/src/main/java/com/cubearrow/model/regex/OperationSelector.java
+++ model/src/main/java/com/cubearrow/model/regex/OperationSelector.java
@@ -1,43 +1,33 @@
 package com.cubearrow.model.regex;
 
-import com.cubearrow.model.operations.Operation;
-import org.reflections.Reflections;
+import com.cubearrow.model.operations.*;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.function.IntFunction;
 
 public class OperationSelector {
-    private final HashMap<String, Class<Operation>> operationHashMap;
-
-    /**
-     * Returns the {@link LinkedHashMap} that represents the operations and their operation string
-     * @return Return the {@link LinkedHashMap}
-     */
-    public HashMap<String, Class<Operation>> getOperationHashMap() {
-        return operationHashMap;
-    }
+    private final static Class<? extends Operation>[] operationClasses = new Class[]{Addition.class, Division.class, Multiplication.class, Subtraction.class};
+    private final HashMap<String, Class<? extends Operation>> operationHashMap = new HashMap<>();
 
     /**
      * Initializes an OperationSelector by adding all of the subclasses of {@link Operation} to a HashMap
      */
-    @SuppressWarnings("unchecked")
     public OperationSelector() {
-        HashMap<String, Class<Operation>> operationHashMap = new HashMap<>();
-
-        Reflections reflection = new Reflections("com.cubearrow.model.operations");
-        Set<Class<? extends Operation>> operationClasses = reflection.getSubTypesOf(Operation.class);
-        operationClasses.forEach(operationClass -> {
+        for (Class<? extends Operation> operationClass : operationClasses) {
             try {
-                operationHashMap.put((String) operationClass.getDeclaredField("OPERATION_STRING").get(null), (Class<Operation>) operationClass);
+                operationHashMap.put((String) operationClass.getDeclaredField("OPERATION_STRING").get(null), operationClass);
             } catch (NoSuchFieldException | IllegalAccessException e) {
                 e.printStackTrace();
             }
-        });
-        this.operationHashMap = operationHashMap;
+        }
     }
 
     /**
      * Sorts the provided classes by the priority field and return it in an Array of Classes
+     *
      * @param classes The classes to be sorted
      * @return Returns the sorted array
      */
@@ -53,11 +43,21 @@ public class OperationSelector {
     }
 
     /**
+     * Returns the {@link LinkedHashMap} that represents the operations and their operation string
+     *
+     * @return Return the {@link LinkedHashMap}
+     */
+    public HashMap<String, Class<? extends Operation>> getOperationHashMap() {
+        return operationHashMap;
+    }
+
+    /**
      * Returns the Class that is represented by an operationString like "+"
+     *
      * @param operation The operation String to search by
      * @return the representing Class
      */
-    public Class<Operation> getOperationFromOperationString(String operation) {
+    public Class<? extends Operation> getOperationFromOperationString(String operation) {
         return this.operationHashMap.get(operation);
     }
 }
