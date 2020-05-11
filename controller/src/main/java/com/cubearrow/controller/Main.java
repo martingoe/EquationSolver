2a84abe245df8b2d44d353e57f90a52f9e3c2a09
xCubeArrow <1984flaker@gmail.com>
2020-05-11 23:34:26 +0200

    Update the testing Main.java class

--- controller/src/main/java/com/cubearrow/controller/Main.java
+++ controller/src/main/java/com/cubearrow/controller/Main.java
@@ -2,24 +2,19 @@ package com.cubearrow.controller;
 
 import com.cubearrow.model.equation.Equation;
 import com.cubearrow.model.equation.EquationInitializer;
-import com.cubearrow.model.operations.Operation;
 import com.cubearrow.model.rewriting.EquationRewriter;
-import com.cubearrow.model.tree.Variable;
 import com.cubearrow.view.Interaction;
 
 public class Main {
     public static void main(String[] args) {
-        Equation test = new EquationInitializer("((3/4)*x)+x=2+x").parseEquation();
+//        Equation test = new EquationInitializer("((3/4)*x)+x=2+x").parseEquation();
 
+        Equation test = new EquationInitializer("(4*4)*x=(2x)+5").parseEquation();
         Interaction interaction = new Interaction(test);
-        test.setVariableToIsolate(interaction.askUserForVariableToIsolate());
         EquationRewriter equationRewriter = new EquationRewriter();
-        for (int i = 0; i < 20; i++) {
+        for (int j = 0; j < 20; j++) {
             test = test.simplify(equationRewriter);
         }
         interaction.displayResult(test);
-        //OperationSelector.sortClassesByPriority(operationSelector.getOperationHashMap().values().toArray(new Class[0]));
-
-        System.out.println("DEBUG");
     }
 }
