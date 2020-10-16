1c3f5db074eeca0169b8c7d9d8237834d3d0feb5
xCubeArrow <1984flaker@gmail.com>
2020-10-16 11:25:52 +0200

    Don't use the web server

--- controller/src/main/java/com/cubearrow/controller/Main.java
+++ controller/src/main/java/com/cubearrow/controller/Main.java
@@ -1,10 +1,23 @@
 package com.cubearrow.controller;
 
+import com.cubearrow.model.problem.Problem;
+import com.cubearrow.model.problem.ProblemManager;
+import com.cubearrow.model.tree.Node;
+import com.cubearrow.view.Interaction;
 import de.cubearrow.restview.services.ProblemService;
 import org.springframework.boot.SpringApplication;
 
 public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(ProblemService.class);
+        Problem test = Problem.fromString("1/4 * (4 - 1/2)", new Problem.ProblemConfig(false));
+        Interaction interaction = new Interaction(test);
+        ProblemManager problemManager = new ProblemManager();
+        Node prevNode = null;
+        while (prevNode != test.getTopNode()) {
+            prevNode = test.getTopNode();
+            test.setTopNode(problemManager.solveProblem(test));
+        }
+        interaction.displayResult(test.getTopNode());
+
     }
 }
